3390359c471313a590317f1147d9fe0a69e57487
Felix Tesche <felix.tesche@gmail.com>
2021-09-22 12:51:12 +0200

    added new course files for WS2021/2022

--- src/components/authentication.vue
+++ src/components/authentication.vue
@@ -45,7 +45,7 @@
             </div>
             <div v-else>
                 <!-- <h4>Leider gibt es Probleme mit der Datenbank, versuche es später noch einmal.</h4> -->
-                <h4>Das hier ist die Entwicklerversion. Die Anmeldung folgt kommende Woche</h4>
+                <h4>Das hier ist die Entwicklerversion. Die Anmeldung folgt.</h4>
             </div>
         </div>
     </div>
